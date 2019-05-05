2b812fba1757e7b70c3139c4b561b539101c6e43
Rylan1 <rylanbotelho@hotmail.co.uk>
2019-05-05 21:43:19 +0100

    5th

---
+++ app/src/main/java/com/example/nlp_sch/DisplaySch.java
@@ -0,0 +1,13 @@
+package com.example.nlp_sch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplaySch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_sch);
+    }
+}
